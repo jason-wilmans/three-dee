e918a746645eed83a97be76e97d981b1f4a9bb72
jason <jason.wilmans@haw-hamburg.de>
2016-01-12 17:40:56 +0100

    Began Ellipsoid

---
+++ 1 - Code/ThreeDeeCore/CoreComponents/DiagramLogic/Interface/Elements/Ellipsoid.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ZeroTypes;
+
+namespace DiagramLogic.Interface.Elements
+{
+    public class Ellipsoid : IDiagramElement
+    {
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Tuple3 Position { get; set; }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Tuple3 Rotation { get; set; }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public Tuple3 Scale { get; set; }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        public IDiagramElement Parent { get; set; }
+
+        /// <summary>
+        /// <inheritdoc />
+        /// </summary>
+        /// <returns></returns>
+        public IDiagramElement CreateCopy()
+        {
+            return new Ellipsoid
+            {
+                Id = Id,
+                Position = Position,
+                Rotation = Rotation,
+                Scale = Scale,
+                Parent = Parent
+            };
+        }
+    }
+}
