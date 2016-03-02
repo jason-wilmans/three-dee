ed6cbd25e3508f88edf0908a4449d1145cfcd69c
Jason <jason@crabba.de>
2016-03-02 01:02:30 +0100

    dfgdf

---
+++ 1 - Code/ThreeDee/ThreeDee/ThreeDee.Game/MouseLookScript.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Engine;
+
+namespace ThreeDee
+{
+    public class MouseLookScript : SyncScript
+    {
+        public override void Update()
+        {
+            //Entity.Transform.RotationEulerXYZ = new Vector3(
+            //    Entity.Transform.RotationEulerXYZ.X,
+            //    Entity.Transform.RotationEulerXYZ.Y + 0.01f,
+            //    Entity.Transform.RotationEulerXYZ.Z);
+        }
+    }
+}
--- 1 - Code/ThreeDee/ThreeDee/ThreeDee.Windows/ThreeDeeApp.cs
+++ 1 - Code/ThreeDee/ThreeDee/ThreeDee.Windows/ThreeDeeApp.cs
@@ -8,6 +8,9 @@ namespace ThreeDee
         {
             using (var game = new Game())
             {
+                game.GraphicsDeviceManager.PreferredBackBufferWidth = 1920;
+                game.GraphicsDeviceManager.PreferredBackBufferHeight = 1080;
+                //game.GraphicsDeviceManager.IsFullScreen = false;
                 game.Run();
             }
         }
