8aba67d1aeef9b6c83f156ded4f55f67eefe1dac
Jason <jason@crabba.de>
2016-03-24 02:22:05 +0100

    lknj

---
+++ 1 - Code/ThreeDee/ThreeDee/ThreeDee.Game/CameraRigScript.cs
@@ -0,0 +1,62 @@
+using SiliconStudio.Xenko.Engine;
+using System;
+using SiliconStudio.Core;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Input;
+
+namespace ThreeDee
+{
+    [DataContract]
+    public class CameraRigScript : SyncScript
+    {
+        [DataMember]
+        public TransformComponent JointTransform;
+
+        private const float ScrollDelta = 0.005f;
+        private const float ScrollingSpeed = 0.05f;
+        private const float PanSpeed = 1.0f;
+
+        private TransformComponent _transform;
+        private Vector2 _oldMousePosition;
+        
+        public override void Start()
+        {
+            _transform = Entity.GetOrCreate<TransformComponent>();
+        }
+
+        public override void Update()
+        {
+            Scrolling();
+            Panning();
+        }
+        
+        #region Scrolling
+
+        public void Scrolling()
+        {
+            if (Math.Abs(Input.MouseWheelDelta) > ScrollDelta)
+            {
+                var zoomDelta = JointTransform.WorldMatrix.Forward * Input.MouseWheelDelta * ScrollingSpeed;
+                _transform.Position += zoomDelta * ScrollingSpeed;
+            }
+        }
+
+        #endregion
+        
+        #region Panning
+
+        private void Panning()
+        {
+            if (Input.IsMouseButtonDown(MouseButton.Left) && Input.IsMouseButtonDown(MouseButton.Right))
+            {
+                Vector2 delta = (Input.MousePosition - _oldMousePosition) * PanSpeed;
+                _transform.Position += JointTransform.WorldMatrix.Right*delta.X;
+                _transform.Position += JointTransform.WorldMatrix.Up* delta.Y;
+            }
+
+            _oldMousePosition = Input.MousePosition;
+        }
+
+        #endregion
+    }
+}
