947c420aee0dd723ce00b3e5e702963b755b9f05
Jason <jason@crabba.de>
2016-03-30 20:58:05 +0200

    Started XenkoUtilities Project

--- 1 - Code/ThreeDee/ThreeDee/ThreeDee.Game/Focus.cs
+++ 1 - Code/ThreeDee/ThreeDee/ThreeDee.Game/Focus.cs
@@ -1,37 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SiliconStudio.Core.Mathematics;
-using SiliconStudio.Xenko.Animations;
-using SiliconStudio.Xenko.Engine;
-using SiliconStudio.Xenko.Graphics;
-using SiliconStudio.Xenko.Graphics.Font;
-using SiliconStudio.Xenko.UI;
-using SiliconStudio.Xenko.UI.Controls;
-using SiliconStudio.Xenko.UI.Panels;
+﻿using SiliconStudio.Xenko.Engine;
+using XenkoUtilities;
 
 namespace ThreeDee
 {
     public class Focus : SyncScript
     {
-        public SpriteFont Font { get; set; }
         public TransformComponent Target;
-        private UIComponent _ui;
-        private TextBlock _angle;
-        private TextBlock _dot;
-        private TextBlock _over;
 
         public override void Update()
         {
-            Vector3 lookDirection = Target.Position - Entity.Transform.Position;
-            double horizontalAngle = Math.Atan2(lookDirection.Z, lookDirection.X);
-            double verticalAngle = Math.Atan2(lookDirection.Y, lookDirection.Z);
-
-            Quaternion combined = Quaternion.RotationYawPitchRoll((float)-horizontalAngle - MathUtil.PiOverTwo, (float) (-verticalAngle + Math.PI), 0);
-
-            Entity.Transform.Rotation = combined;
+            Entity.Transform.LookAt(Target.Position);
         }
     }
 }
---
+++ 1 - Code/ThreeDee/ThreeDee/XenkoUtilities/TransformComponentExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Engine;
+
+namespace XenkoUtilities
+{
+    public static class TransformComponentExtensions
+    {
+        public static void LookAt(this TransformComponent transform, Vector3 target)
+        {
+            Vector3 lookDirection = target - transform.Position;
+            double horizontalAngle = Math.Atan2(lookDirection.Z, lookDirection.X);
+            double verticalAngle = Math.Atan2(lookDirection.Y, lookDirection.Z);
+            
+            transform.Rotation = Quaternion.RotationYawPitchRoll((float)-horizontalAngle - MathUtil.PiOverTwo, (float)(-verticalAngle + Math.PI), 0);
+        }
+    }
+}
