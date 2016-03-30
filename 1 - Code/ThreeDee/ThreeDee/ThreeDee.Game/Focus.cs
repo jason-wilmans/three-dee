36d1b65053ea2ac066f26b19e762d355e32831ee
Jason <jason@crabba.de>
2016-03-30 20:44:04 +0200

    Vertical as well

--- 1 - Code/ThreeDee/ThreeDee/ThreeDee.Game/Focus.cs
+++ 1 - Code/ThreeDee/ThreeDee/ThreeDee.Game/Focus.cs
@@ -23,61 +23,15 @@ namespace ThreeDee
         private TextBlock _dot;
         private TextBlock _over;
 
-        public override void Start()
-        {
-            base.Start();
-
-            //StackPanel panel = new StackPanel
-            //{
-            //    Orientation = Orientation.Horizontal
-            //};
-
-            //_ui.RootElement = panel;
-
-            //SetupLabels(panel);
-        }
-
         public override void Update()
         {
             Vector3 lookDirection = Target.Position - Entity.Transform.Position;
             double horizontalAngle = Math.Atan2(lookDirection.Z, lookDirection.X);
-            double verticalAngle = Math.Atan2(lookDirection.Y, lookDirection.X);
+            double verticalAngle = Math.Atan2(lookDirection.Y, lookDirection.Z);
 
-            Quaternion combined = Quaternion.RotationY((float) -horizontalAngle - MathUtil.PiOverTwo);
+            Quaternion combined = Quaternion.RotationYawPitchRoll((float)-horizontalAngle - MathUtil.PiOverTwo, (float) (-verticalAngle + Math.PI), 0);
 
             Entity.Transform.Rotation = combined;
-        }
-
-        private double Clamp(double f)
-        {
-            return Math.Min(1f, Math.Max(-1, f));
-        }
-
-        private void SetupLabels(StackPanel panel)
-        {
-            _dot = new TextBlock
-            {
-                Font = Font,
-                TextColor = Color.Magenta,
-                Margin = new Thickness(20, 20, 0, 0)
-            };
-            panel.Children.Add(_dot);
-
-            _over = new TextBlock
-            {
-                Font = Font,
-                TextColor = Color.Magenta,
-                Margin = new Thickness(20, 20, 0, 0)
-            };
-            panel.Children.Add(_over);
-
-            _angle = new TextBlock
-            {
-                Font = Font,
-                TextColor = Color.Magenta,
-                Margin = new Thickness(20, 20, 0, 0)
-            };
-            panel.Children.Add(_angle);
         }
     }
 }
