dec39d30a27f690425ca8701e3a1dad94cf9affa
Jason <jason@crabba.de>
2016-05-18 12:14:49 +0200

    didn't pish anyway?

--- 1 - Code/ThreeDeeUi/ThreeDeeUi/Ui3DToolkit/ContainerElement.cs
+++ 1 - Code/ThreeDeeUi/ThreeDeeUi/Ui3DToolkit/ContainerElement.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace UIToolkit
+﻿namespace Ui3DToolkit
 {
     public class ContainerElement : UiElement3D
     {
--- 1 - Code/ThreeDeeUi/ThreeDeeUi/Ui3DToolkit/UIElement3D.cs
+++ 1 - Code/ThreeDeeUi/ThreeDeeUi/Ui3DToolkit/UIElement3D.cs
@@ -1,99 +1,12 @@
-﻿using System;
-using UnityEngine;
+﻿using SiliconStudio.Xenko.UI;
 
-namespace UIToolkit
+namespace Ui3DToolkit
 {
-    public class UiElement3D : MonoBehaviour
+    public class UiElement3D : UIElement
     {
         public UiElement3D()
         {
-            Parent = null;
-        }
-
-        /// <summary>
-        ///     This 3D UI element's parent element.
-        /// </summary>
-        public UiElement3D Parent { get; protected set; }
-
-        /// <summary>
-        ///     Called, if the mouse enters about a collider of this object or one of its children.
-        /// </summary>
-        private event Action<MouseEventArgs> MouseEnter;
-
-        /// <summary>
-        ///     Called, if the mouse exits of a collider of this object or one of its children.
-        /// </summary>
-        private event Action<MouseEventArgs> MouseExit;
-
-        public void OnEnable()
-        {
-            Transform current = GetComponent<Transform>().parent;
-            while (current != null)
-            {
-                UiElement3D[] elements = current.GetComponents<UiElement3D>();
-                if (elements == null || elements.Length == 0)
-                {
-                    current = current.parent;
-                }
-                else
-                {
-                    if (elements.Length > 1)
-                    {
-                        Debug.LogError("Multiple ui elements on same go");
-                        throw new InvalidOperationException("You can't have more than one UIElement3D script on one GameObject: " + transform.gameObject.name);
-                    }
-
-                    Parent = elements[0];
-                }
-            }
-        }
-
-        private void OnMouseEnter()
-        {
-            Bubble(EventType.MouseEnter, new MouseEventArgs(Input.mousePosition));
-        }
-
-        private void OnMouseExit()
-        {
-            Bubble(EventType.MouseEnter, new MouseEventArgs(Input.mousePosition));
-        }
-
-        /// <summary>
-        ///     Invokes the given event on the current level of the visual tree.
-        ///     If the event is not handled after that, invokes the same process a level up the chain.
-        /// </summary>
-        /// <param name="eventType"></param>
-        /// <param name="mouseEventArgs"></param>
-        private void Bubble(EventType eventType, MouseEventArgs mouseEventArgs)
-        {
-            var level = this;
-
-            while (!mouseEventArgs.IsHandled && level != null)
-            {
-                Debug.Log("level: " + level.gameObject.name);
-                InvokeEvent(eventType, mouseEventArgs);
-                level = level.Parent;
-            }
-        }
-
-        protected virtual void InvokeEvent(EventType eventType, MouseEventArgs mouseEventArgs)
-        {
-            switch (eventType)
-            {
-                case EventType.MouseEnter:
-                    if (MouseEnter != null)
-                    {
-                        MouseEnter(mouseEventArgs);
-                    }
-                    break;
-
-                case EventType.MouseExit:
-                    if (MouseExit != null)
-                    {
-                        MouseExit(mouseEventArgs);
-                    }
-                    break;
-            }
+            
         }
     }
 }
