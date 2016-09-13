b0fe69a4813c6b8ca27a8a225f0f6403da32ef80
jason <jason.wilmans@haw-hamburg.de>
2016-09-13 11:17:07 +0200

    Try to build a working project structure and remove old trash. compiles with 3 ui toolkit included

---
+++ 1 - Code/ThreeDee/GUI/UI3D/EventArgs/MouseClickEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using SiliconStudio.Core.Mathematics;
+
+namespace GameClient.GUI.Ui3D.EventArgs
+{
+    public class MouseClickEventArgs : UI3DEventArgs
+    {
+        public Vector2 ScreenPosition { get; }
+
+        public Vector3 WorldHitPoint { get; }
+
+        public MouseClickEventArgs(Vector2 screenPosition, Vector3 worldHitPoint)
+        {
+            ScreenPosition = screenPosition;
+            WorldHitPoint = worldHitPoint;
+        }
+    }
+}
