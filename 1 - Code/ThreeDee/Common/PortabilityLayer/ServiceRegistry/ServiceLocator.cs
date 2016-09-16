55e8c899fed90092561acf3792bcf4f5b63669ab
Jason <jason.wilmans@haw-hamburg.de>
2016-09-16 18:48:25 +0200

    Implemented service locator and got 3d UI Event System up and running

--- 1 - Code/ThreeDee/Common/PortabilityLayer/ServiceRegistry/ServiceLocator.cs
+++ 1 - Code/ThreeDee/Common/PortabilityLayer/ServiceRegistry/ServiceLocator.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PortabilityLayer.IO;
 
 namespace PortabilityLayer.ServiceRegistry
 {
     class ServiceLocator : IServiceLocator
     {
-        public T GetServiceInstance<T>()
+        public T GetServiceInstance<T>() where T : class
+
         {
-            throw new NotImplementedException();
+            var typeName = typeof (T).Name;
+            switch (typeName)
+            {
+                case nameof(IFileAccess):
+                    return new FileAccess() as T;
+                    
+                default:
+                    throw new ArgumentException($"No service compatible to '{typeName}' registered.", nameof(T));
+            }
         }
     }
 }
