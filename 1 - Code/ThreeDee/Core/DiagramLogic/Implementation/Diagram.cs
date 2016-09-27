de28c47822c350318ac50dfde1c467ad89853e38
Jason <jason@crabba.de>
2016-09-28 00:29:13 +0200

    Fixed a bug where calculation of the geometric center of the diagram would return NaN as components when the diagram doesn't contain elements (yet).

--- 1 - Code/ThreeDee/Core/DiagramLogic/Implementation/Diagram.cs
+++ 1 - Code/ThreeDee/Core/DiagramLogic/Implementation/Diagram.cs
@@ -92,6 +92,8 @@ namespace DiagramLogic.Implementation
 
         public Tuple3 CalculateGeometricCenter()
         {
+            if(Elements.Count < 1) return new Tuple3();
+
             double x = 0;
             double y = 0;
             double z = 0;
